d310fbe75c09054ded75f2121ade9d6d29987aa0
Ghadeer <ghadeer@futurecode-co.com>
2023-05-23 11:45:35 +0300

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <BrowserRouter>
+  </BrowserRouter>
+)
